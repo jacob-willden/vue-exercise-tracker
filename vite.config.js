f60c5a22dd6a2555864051cc423c1bc536120e61
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2025-01-03 14:54:36 -0700

    Add JavaScript license table for LibreJS

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	},
+	// © 2024 Rafał Goławski, released under the Universal Permissive License version 1.0
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].js"
+			}
+		}
+	}
 })
